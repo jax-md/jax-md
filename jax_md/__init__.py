de50b44814a4aa149b8554bcd7e2cd9c21bb4454
Sam Schoenholz <schsam@google.com>
2019-05-29 21:33:35 -0700

    Fixed parameterization of LJ potential.

--- jax_md/__init__.py
+++ jax_md/__init__.py
@@ -11,5 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+
 from jax_md import space, energy, minimize, simulate, smap
