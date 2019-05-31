9499cbbe06a7012db3e88b655dcfbd0818f370b5
Ekin Dogus Cubuk <cubuk@google.com>
2019-05-31 16:14:00 -0700

    Adding EAM potential

--- jax_md/energy.py
+++ jax_md/energy.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import jax.numpy as np
 
 from jax_md import space, smap
+from jax_md.interpolate import spline
 
 
 def soft_sphere(dR, sigma=1.0, epsilon=1.0, alpha=2.0):
@@ -84,3 +85,111 @@ def lennard_jones_pairwise(
   """Convenience wrapper to compute Lennard-Jones energy over a system."""
   return smap.pairwise(
       lennard_jones, metric, species=species, sigma=sigma, epsilon=epsilon)
+
+
+def load_lammps_eam_parameters(f):
+  """Reads EAM parameters from a LAMMPS file and returns relevant spline fits.
+
+  This function reads single-element EAM potential fit parameters from a file
+  in DYNAMO funcl format. In summary, the file contains:
+  Line 1-3: comments,
+  Line 4: Number of elements and the element type,
+  Line 5: The number of charge values that the embedding energy is evaluated
+  on (num_drho), interval between the charge values (drho), the number of
+  distances the pairwise energy and the charge density is evaluated on (num_dr),
+  the interval between these distances (dr), and the cutoff distance (cutoff).
+  The lines that come after are the embedding function evaluated on num_drho
+  charge values, charge function evaluated at num_dr distance values, and
+  pairwise energy evaluated at num_dr distance values. Note that the pairwise
+  energy is multiplied by distance (in units of eV x Angstroms). For more
+  details of the DYNAMO file format, see:
+  https://sites.google.com/a/ncsu.edu/cjobrien/tutorials-and-guides/eam
+  Args:
+    f: File handle for the EAM parameters text file.
+
+  Returns:
+    charge_fn: A function that takes an ndarray of shape [n, m] of distances
+      between particles and returns a matrix of charge contributions.
+    embedding_fn: Function that takes an ndarray of shape [n] of charges and
+      returns an ndarray of shape [n] of the energy cost of embedding an atom
+      into the charge.
+    pairwise_fn: A function that takes an ndarray of shape [n, m] of distances
+      and returns an ndarray of shape [n, m] of pairwise energies.
+    cutoff: Cutoff distance for the embedding_fn and pairwise_fn.
+  """
+  raw_text = f.read().split('\n')
+  if 'setfl' not in raw_text[0]:
+    raise ValueError('File format is incorrect, expected LAMMPS setfl format.')
+  temp_params = raw_text[4].split()
+  num_drho, num_dr = int(temp_params[0]), int(temp_params[2])
+  drho, dr, cutoff = float(temp_params[1]), float(temp_params[3]), float(
+      temp_params[4])
+  data = np.array(map(float, raw_text[6:-1]))
+  embedding_fn = spline(data[:num_drho], drho)
+  charge_fn = spline(data[num_drho:num_drho + num_dr], dr)
+  # LAMMPS EAM parameters file lists pairwise energies after multiplying by
+  # distance, in units of eV*Angstrom. We divide the energy by distance below,
+  distances = np.arange(num_dr) * dr
+  # Prevent dividing by zero at zero distance, which will not
+  # affect the calculation
+  distances = np.where(distances == 0, 0.001, distances)
+  pairwise_fn = spline(
+      data[num_dr + num_drho:num_drho + 2 * num_dr] / distances,
+      dr)
+  return charge_fn, embedding_fn, pairwise_fn, cutoff
+
+
+def eam(displacement, charge_fn, embedding_fn, pairwise_fn, axis=None):
+  """Interatomic potential as approximated by embedded atom model (EAM).
+
+  This code implements the EAM approximation to interactions between metallic
+  atoms. In EAM, the potential energy of an atom is given by two terms: a
+  pairwise energy and an embedding energy due to the interaction between the
+  atom and background charge density. The EAM potential for a single atomic
+  species is often
+  determined by three functions:
+    1) Charge density contribution of an atom as a function of distance.
+    2) Energy of embedding an atom in the background charge density.
+    3) Pairwise energy.
+  These three functions are usually provided as spline fits, and we follow the
+  implementation and spline fits given by [1]. Note that in current
+  implementation, the three functions listed above can also be expressed by a
+  any function with the correct signature, including neural networks.
+
+  Args:
+    displacement: A function that produces an ndarray of shape [n, m,
+      spatial_dimension] of particle displacements from particle positions
+      specified as an ndarray of shape [n, spatial_dimension] and [m,
+      spatial_dimension] respectively.
+    charge_fn: A function that takes an ndarray of shape [n, m] of distances
+      between particles and returns a matrix of charge contributions.
+    embedding_fn: Function that takes an ndarray of shape [n] of charges and
+      returns an ndarray of shape [n] of the energy cost of embedding an atom
+      into the charge.
+    pairwise_fn: A function that takes an ndarray of shape [n, m] of distances
+      and returns an ndarray of shape [n, m] of pairwise energies.
+    axis: Specifies which axis the total energy should be summed over.
+
+  Returns:
+    A function that computes the EAM energy of a set of atoms with positions
+    given by an [n, spatial_dimension] ndarray.
+
+  [1] Y. Mishin, D. Farkas, M.J. Mehl, DA Papaconstantopoulos, "Interatomic
+  potentials for monoatomic metals from experimental data and ab initio
+  calculations." Physical Review B, 59 (1999)
+  """
+  def energy(R, **kwargs):
+    dr = space.distance(displacement(R, R, **kwargs))
+    total_charge = smap._high_precision_sum(charge_fn(dr), axis=1)
+    embedding_energy = embedding_fn(total_charge)
+    pairwise_energy = smap._high_precision_sum(smap._diagonal_mask(
+        pairwise_fn(dr)), axis=1) / 2.0
+    return smap._high_precision_sum(
+        embedding_energy + pairwise_energy, axis=axis)
+
+  return energy
+
+
+def eam_from_lammps_parameters(displacement, f):
+  """Convenience wrapper to compute EAM energy over a system."""
+  return eam(displacement, *load_lammps_eam_parameters(f)[:-1])
--- tests/energy_test.py
+++ tests/energy_test.py
@@ -25,12 +25,15 @@ from jax.config import config as jax_config
 from jax import random
 import jax.numpy as np
 
+import numpy as onp
+
 from jax.api import grad
 from jax_md import space
 
 from jax import test_util as jtu
 
 from jax_md import energy
+from jax_md.interpolate import spline
 
 jax_config.parse_flags_with_absl()
 FLAGS = jax_config.FLAGS
@@ -38,8 +41,55 @@ FLAGS = jax_config.FLAGS
 PARTICLE_COUNT = 10
 STOCHASTIC_SAMPLES = 10
 SPATIAL_DIMENSION = [2, 3]
+UNIT_CELL_SIZE = [7, 8]
 
 SOFT_SPHERE_ALPHA = [2.0, 3.0]
+
+
+def lattice_repeater(small_cell_pos, latvec, no_rep):
+  pos = onp.copy(small_cell_pos).tolist()
+  for atom in small_cell_pos:
+    for i in range(no_rep):
+      for j in range(no_rep):
+        for k in range(no_rep):
+          if not i == j == k == 0:
+            repeated_atom = atom + latvec[0] * i + latvec[1] * j + latvec[2] * k
+            pos.append(onp.array(repeated_atom).tolist())
+  return np.array(pos), latvec*no_rep
+
+CUTOFF = 6.28721
+
+NUM_SPLINE_POINTS = 21
+dr = np.arange(0, NUM_SPLINE_POINTS) * (CUTOFF / NUM_SPLINE_POINTS)
+drho = np.arange(0, 2, 2. / NUM_SPLINE_POINTS)
+
+DENSITY_DATA = np.array([2.78589606e-01, 2.02694937e-01, 1.45334053e-01,
+                      1.06069912e-01, 8.42517168e-02, 7.65140344e-02,
+                      7.76263116e-02, 8.23214224e-02, 8.53322309e-02,
+                      8.13915861e-02, 6.59095390e-02, 4.28915711e-02,
+                      2.27910928e-02, 1.13713167e-02, 6.05020311e-03,
+                      3.65836583e-03, 2.60587564e-03, 2.06750708e-03,
+                      1.48749693e-03, 7.40019174e-04, 6.21225205e-05])
+
+EMBEDDING_DATA = np.array([1.04222211e-10, -1.04142633e+00, -1.60359806e+00,
+                       -1.89287637e+00, -2.09490167e+00, -2.26456628e+00,
+                       -2.40590322e+00, -2.52245359e+00, -2.61385603e+00,
+                       -2.67744693e+00, -2.71053295e+00, -2.71110418e+00,
+                       -2.69287013e+00, -2.68464527e+00, -2.69204083e+00,
+                       -2.68976209e+00, -2.66001244e+00, -2.60122024e+00,
+                       -2.51338548e+00, -2.39650817e+00, -2.25058831e+00])
+PAIRWISE_DATA = np.array([6.27032242e+01, 3.49638589e+01, 1.79007014e+01,
+                  8.69001383e+00, 4.51545250e+00, 2.83260884e+00,
+                  1.93216616e+00, 1.06795515e+00, 3.37740836e-01,
+                  1.61087890e-02, -6.20816372e-02, -6.51314297e-02,
+                  -5.35210341e-02, -5.20950200e-02, -5.51709524e-02,
+                  -4.89093894e-02, -3.28051688e-02, -1.13738785e-02,
+                  2.33833655e-03, 4.19132033e-03, 1.68600692e-04])
+
+
+charge_fn = spline(DENSITY_DATA, dr[1] - dr[0])
+embedding_fn = spline(EMBEDDING_DATA, drho[1] - drho[0])
+pairwise_fn = spline(PAIRWISE_DATA, dr[1] - dr[0])
 
 
 class EnergyTest(jtu.JaxTestCase):
@@ -97,5 +147,22 @@ class EnergyTest(jtu.JaxTestCase):
       self.assertAllClose(g, np.zeros_like(g), True)
 
 
+  @parameterized.named_parameters(jtu.cases_from_list(
+      {
+          'testcase_name': '_dim={}'.format(num_repetitions),
+          'num_repetitions': num_repetitions,
+      } for num_repetitions in UNIT_CELL_SIZE))
+  def test_eam(self, num_repetitions):
+    latvec = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]) * 4.05 / 2
+    atoms = np.array([[0, 0, 0]])
+    atoms_repeated, latvec_repeated = lattice_repeater(atoms, latvec, num_repetitions)
+    inv_latvec = np.array(onp.linalg.inv(onp.array(latvec_repeated)))
+    displacement, shift = space.periodic_general(latvec_repeated)
+    eam_energy = energy.eam(displacement, charge_fn, embedding_fn, pairwise_fn)
+    self.assertAllClose(
+        eam_energy(np.dot(atoms_repeated, inv_latvec))/num_repetitions**3, -3.366, True)
+
+
 if __name__ == '__main__':
   absltest.main()
+
