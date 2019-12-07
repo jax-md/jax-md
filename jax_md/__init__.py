6648e9cb68da5147e75dbe420fbd1418b4d0f3f4
Sam Schoenholz <schsam@google.com>
2019-12-07 11:24:16 -0800

    Added partition to __init__.py.

--- jax_md/__init__.py
+++ jax_md/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from jax_md import space, energy, minimize, simulate, smap
+from jax_md import space, energy, minimize, simulate, smap, partition
