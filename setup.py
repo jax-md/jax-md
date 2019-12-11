b0ba800ca566e8b855a81e3fe8c40eaabdf611ef
sschoenholz <schsam@google.com>
2019-12-11 12:08:51 -0800

    setup.py fix.

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import io
+import os
 import setuptools
 
 # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
