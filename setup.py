3ddc4c74af190ea5827e0ae0b0546e9a2d9c2279
Sam Schoenholz <schsam@google.com>
2020-04-15 11:44:50 -0700

    Bump pypi version.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name='jax-md',
-    version='0.1.2',
+    version='0.1.3',
     license='Apache 2.0',
     author='Google',
     author_email='jax-md-dev@google.com',
