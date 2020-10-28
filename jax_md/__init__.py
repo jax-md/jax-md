2eb03f81de6aae6807bb0e7a6032a8b7b1094d69
sschoenholz <schsam@sas.upenn.edu>
2020-10-27 21:00:45 -0700

    Update __init__.py

--- jax_md/__init__.py
+++ jax_md/__init__.py
@@ -22,5 +22,9 @@ from jax_md import dataclasses
 from jax_md import nn
 from jax_md import interpolate
 from jax_md import util
+
 try:
+  # Attempt to load colab_tools if IPython is installed.
   from jax_md import colab_tools
+except:
+  pass
