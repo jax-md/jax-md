bbdbf075f7ba60a5fbfc41289b196f62c919904d
Sam Schoenholz <schsam@google.com>
2021-06-25 21:56:11 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name='jax-md',
-    version='0.1.15',
+    version='0.1.16',
     license='Apache 2.0',
     author='Google',
     author_email='jax-md-dev@google.com',
