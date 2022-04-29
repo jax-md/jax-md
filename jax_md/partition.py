Fix neighbor list capacity and overflow check when nearly full. Add regression tests

jax_md/partition.py
@@ -573,16 +573,16 @@ def neighbor_list(displacement_or_metric: DisplacementOrMetricFn,
 
      init_fn, apply_fn = simulate.nve(energy_fn, shift, 1e-3)
      exact_init_fn, exact_apply_fn = simulate.nve(exact_energy_fn, shift, 1e-3)
-  
+
      nbrs = neighbor_fn.allocate(R)
      state = init_fn(random.PRNGKey(0), R, neighbor_idx=nbrs.idx)
-  
+
      def body_fn(i, state):
        state, nbrs = state
        nbrs = nbrs.update(state.position)
        state = apply_fn(state, neighbor_idx=nbrs.idx)
        return state, nbrs
-  
+
      step = 0
      for _ in range(20):
        new_state, nbrs = lax.fori_loop(0, 100, body_fn, (state, nbrs))
@@ -640,7 +640,7 @@ def neighbor_list(displacement_or_metric: DisplacementOrMetricFn,
   cell_size = cutoff
   if fractional_coordinates:
     cell_size = cutoff / box_size
-    box_size = (f32(box_size) if onp.isscalar(box_size) 
+    box_size = (f32(box_size) if onp.isscalar(box_size)
                 else onp.ones_like(box_size, f32))
 
   use_cell_list = jnp.all(cell_size < box_size / 3.) and not disable_cell_list
@@ -702,7 +702,7 @@ def neighbor_list(displacement_or_metric: DisplacementOrMetricFn,
     out_idx = out_idx.at[p_index, index].set(idx)
     max_occupancy = jnp.max(cumsum[:, -1])
 
-    return out_idx[:, :-1], max_occupancy
+    return out_idx, max_occupancy
 
   @jit
   def prune_neighbor_list_sparse(position: Array, idx: Array, **kwargs
@@ -729,7 +729,7 @@ def neighbor_list(displacement_or_metric: DisplacementOrMetricFn,
     sender_idx = out_idx.at[index].set(sender_idx)
     max_occupancy = cumsum[-1]
 
-    return jnp.stack((receiver_idx[:-1], sender_idx[:-1])), max_occupancy
+    return jnp.stack((receiver_idx, sender_idx)), max_occupancy
 
   def neighbor_list_fn(position: Array,
                        neighbors: Optional[NeighborList] = None,
@@ -766,19 +766,23 @@ def neighbor_list(displacement_or_metric: DisplacementOrMetricFn,
         _extra_capacity = (extra_capacity if not is_sparse(format)
                            else N * extra_capacity)
         max_occupancy = int(occupancy * capacity_multiplier + _extra_capacity)
-        if max_occupancy > N and not is_sparse(format):
-          max_occupancy = N - 1 if mask_self else N
-        if max_occupancy > occupancy:
-          padding = max_occupancy - occupancy
-          pad = N * jnp.ones((idx.shape[0], padding), dtype=idx.dtype)
-          idx = jnp.concatenate([idx, pad], axis=1)
+        if max_occupancy > idx.shape[-1]:
+          max_occupancy = idx.shape[-1]
+        if not is_sparse(format):
+          capacity_limit = N - 1 if mask_self else N
+        elif format is NeighborListFormat.Sparse:
+          capacity_limit = N * (N - 1) if mask_self else N**2
+        else:
+          capacity_limit = N * (N - 1) // 2
+        if max_occupancy > capacity_limit:
+          max_occupancy = capacity_limit
       idx = idx[:, :max_occupancy]
       update_fn = (neighbor_list_fn if neighbors is None else
                    neighbors.update_fn)
       return NeighborList(
           idx,
           position,
-          overflow | (occupancy >= max_occupancy),
+          overflow | (occupancy > max_occupancy),
           cl_capacity,
           max_occupancy,
           format,

tests/partition_test.py
@@ -377,7 +377,7 @@ class NeighborListTest(test_util.JAXMDTestCase):
 
     def acceptable_id_pair(id1, id2):
       '''
-      Don't allow particles to have an interaction when their id's 
+      Don't allow particles to have an interaction when their id's
       are closer than 3 (eg disabling 1-2 and 1-3 interactions)
       '''
       return jnp.abs(id1-id2)>3
@@ -406,12 +406,12 @@ class NeighborListTest(test_util.JAXMDTestCase):
 
     ids = jnp.arange(n_particles) # id is just particle index here.
     mask_val = n_particles
-    custom_mask_function = partial(mask_id_based, 
-      ids=ids, 
-      mask_val=mask_val, 
+    custom_mask_function = partial(mask_id_based,
+      ids=ids,
+      mask_val=mask_val,
       _acceptable_id_pair=acceptable_id_pair
     )
-    
+
     neighbor_list_fn = partition.neighbor_list(
       displacement_fn,
       box_size=box_size,
@@ -427,6 +427,72 @@ class NeighborListTest(test_util.JAXMDTestCase):
     With masking -> 42.
     '''
     self.assertEqual(42, (neighbors.idx!=mask_val).sum())
+
+  def test_issue191_1(self):
+    box_vector = jnp.ones(3) * 3
+
+    r_cut = 0.1
+    _positions = jnp.linspace(0.5, 0.7, 20)
+    positions = jnp.stack([_positions, _positions, _positions], axis=1)
+
+    displacement, _ = space.periodic_general(box_vector)
+
+    neighbor_fn = partition.neighbor_list(displacement, box_vector, r_cut,
+                                          0.1 * r_cut,
+                                          fractional_coordinates=True)
+
+    neighbor2_fn = partition.neighbor_list(displacement, box_vector[0], r_cut,
+                                          0.1 * r_cut,
+                                          fractional_coordinates=True,
+                                          disable_cell_list=True)
+
+    nbrs = neighbor_fn.allocate(positions)
+    nbrs2 = neighbor2_fn.allocate(positions)
+    self.assertAllClose(nbrs, nbrs)
+
+  @parameterized.named_parameters(test_util.cases_from_list(
+    {
+      'testcase_name': f'_case={i}_mask_self={ms}_format={fmt.name}',
+      'r_cut': r_cut,
+      'disable_cell_list': dc,
+      'capacity_multiplier': cm,
+      'mask_self': ms,
+      'format': fmt,
+    } for i, (r_cut, dc, cm) in enumerate(
+      [(0.12, True, 1.5), (0.25, False, 1.5),
+       (0.31, False, 1.5), (0.31, False, 1.0)]) for ms in [False, True]
+      for fmt in [partition.Dense, partition.Sparse, partition.OrderedSparse]))
+  def test_issue191_2(self, r_cut, disable_cell_list, capacity_multiplier,
+                      mask_self, format):
+    box = jnp.ones(3)
+    if format is partition.Dense:
+      desired_shape = (20, 19) if mask_self else (20, 20)
+      _positions = jnp.linspace(0.5, 0.7, 20)
+    elif format is partition.Sparse:
+      desired_shape = (2, 20 * 19) if mask_self else (2, 20**2)
+      _positions = jnp.ones((20,)) * 0.5
+    elif format is partition.OrderedSparse:
+      desired_shape = (2, 20 * 19 // 2)
+      _positions = jnp.ones((20,)) * 0.5
+    positions = jnp.stack([_positions, _positions, _positions], axis=1)
+    displacement, _ = space.periodic(box)
+
+    neighbor_fn = partition.neighbor_list(
+      displacement, box,
+      r_cut, 0.1 * r_cut,
+      capacity_multiplier=capacity_multiplier,
+      disable_cell_list=disable_cell_list,
+      mask_self=mask_self,
+      format=format)
+    nbrs = neighbor_fn.allocate(positions)
+
+    self.assertFalse(nbrs.did_buffer_overflow)
+    self.assertEqual(nbrs.idx.shape, desired_shape)
+    new_nbrs = nbrs.update(positions)
+    self.assertFalse(new_nbrs.did_buffer_overflow)
+    self.assertEqual(new_nbrs.idx.shape, desired_shape)
+
+
 
 if __name__ == '__main__':
   absltest.main()