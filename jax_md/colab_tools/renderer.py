7e7f096df2e26372a29991f814c94f62ec9bba8c
Sam Schoenholz <schsam@google.com>
2022-07-08 02:22:11 -0700

    Switch visualization.html from master to main.

--- jax_md/colab_tools/renderer.py
+++ jax_md/colab_tools/renderer.py
@@ -35,7 +35,7 @@ import numpy as np
 
 
 renderer_code = IPython.display.HTML(
-  url=('https://raw.githubusercontent.com/google/jax-md/master/'
+  url=('https://raw.githubusercontent.com/google/jax-md/main/'
        'jax_md/colab_tools/visualization.html')
 )
 
