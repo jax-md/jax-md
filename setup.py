300c5d3b9b6dab52a2f178976f90e577e510e749
Sam Schoenholz <schsam@google.com>
2022-07-17 15:54:28 -0700

    Fix to EAM potential in the presence of keyword arguments.
    Remove redundent import of Optional.
    Add code to parse EAM parameters with six columns (suggested by @emilannevelink).

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name='jax-md',
-    version='0.1.28',
+    version='0.1.29',
     license='Apache 2.0',
     author='Google',
     author_email='jax-md-dev@google.com',
