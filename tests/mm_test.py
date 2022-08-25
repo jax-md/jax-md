cc099f51c5d4df8033e7e331d0cb45db113ac479
dominicrufa <dominic.rufa@gmail.com>
2022-08-24 20:35:22 -0400

    add dense mask test

--- tests/mm_test.py
+++ tests/mm_test.py
@@ -39,7 +39,10 @@ NEIGHBOR_LIST_FORMAT = [partition.Dense,
                         partition.Sparse,
                         partition.OrderedSparse]
 
-PDB_FILENAMES = ['alanine-dipeptide-explicit.pdb']
+PDB_FILENAMES = [
+  'alanine-dipeptide-explicit.pdb',
+  '1yi6-minimize.pdb'
+                 ]
 PBCS_BOOLEAN = [False] # omit True until dsf/PME is sorted
 
 if FLAGS.jax_enable_x64:
@@ -56,14 +59,32 @@ except Exception as exception:
   print(exception, False)
   print(exception)
 
+# utilities
+
+def render_mm_objs_from_pdb(
+  pdb_path: str,
+  ff_files: Iterable[str] = ['amber14-all.xml',
+                             'amber14/tip3pfb.xml'],
+  delete_waters: bool=False,
+  create_system_kwargs: Dict={'nonbondedMethod': app.NoCutoff,
+                              'constraints': None,
+                              'rigidWater': False,
+                              'removeCMMotion': False}) -> Tuple[
+                              openmm.System, Array, openmm.Topology
+                              ]:
+  """render an `openmm.System`, positions, and `openmm.Topology`"""
+  pdb = app.PDBFile(pdb_path)
+  ff = app.ForceField(ff_files)
+  model = app.Modeller(pdb.topology, pdb.positions)
+  if delete_waters:
+      model.deleteWater()
+  mmSystem = ff.createSystem(
+    **create_system_kwargs
+    )
+  return mmSystem, model.positions, model.topology
+
+
 class MMTest(test_util.JAXMDTestCase):
-    # TODO : make a class to handle omm loading utilities
-    # TODO : place each omm.Force into a different ForceGroup to make energy assertions on a Force-specific basis (this functionality is in `perses`)
-    # TODO : write a test to check jit recompilation for different `particle` entries in parameters
-    # TODO : write a test to check energy change assertions for non `particle` parameter adjustement (nothing should be `partialed`)
-    # TODO : check energy error (this is likely a consequence of f32 use)
-    # TODO : allow for **parameters in the energy computation
-    # TODO : assert that not passing `parameters` to energy fn uses `parameters` that the `mm_energy_fn` was initiated with.
 
   @parameterized.named_parameters(test_util.cases_from_list(
       {
@@ -77,24 +98,18 @@ class MMTest(test_util.JAXMDTestCase):
     """assert that the vacuum energy of a solvent-stripped
         `openmm.System` object matches that in `jax_md`
     """
-    pdb = app.PDBFile('data/alanine-dipeptide-explicit.pdb')
-    ff = app.ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
-    model = app.Modeller(pdb.topology, pdb.positions)
-    model.deleteWater()
-    mmSystem = ff.createSystem(model.topology,
-        nonbondedMethod=app.NoCutoff,
-        constraints=None,
-        rigidWater=False,
-        removeCMMotion=False)
+    pdb_filepath = (f"data/{pdb_filename}")
+    mmSystem, positions, topology = render_mm_objs_from_pdb(
+      pdb_path=pdb_filepath,
+      delete_waters=True)
     context = openmm.Context(mmSystem,
         openmm.VerletIntegrator(1.*unit.femtoseconds))
-    context.setPositions(model.getPositions())
+    context.setPositions(positions)
     omm_state = context.getState(getEnergy=True, getPositions=True)
     positions = jnp.array(omm_state.getPositions(asNumpy=True).\
         value_in_unit_system(unit.md_unit_system))
     omm_energy = omm_state.getPotentialEnergy().\
         value_in_unit_system(unit.md_unit_system)
-
     params = mm_utils.parameters_from_openmm_system(mmSystem)
     displacement_fn, shift_fn = space.free()
     energy_fn, neighbor_list_fn = mm.mm_energy_fn(
@@ -103,6 +118,40 @@ class MMTest(test_util.JAXMDTestCase):
 
     jax_energy = energy_fn(positions)
     self.assertAllClose(jax_energy, omm_energy)
+
+    @parameterized.named_parameters(test_util.cases_from_list(
+        {
+            'testcase_name': '_pdb_filename={}_dtype={}'.format(pdb_filename, dtype.__name__),
+            'pdb_filename': pdb_filename,
+            'dtype': dtype
+        } for pdb_filename in PDB_FILENAMES \
+          for dtype in POSITION_DTYPE))
+    def test_dense_exception_mask(self, pdb_filename, dtype):
+      """assert that the jax dense mask renders the same mask as
+      the manual list comprehension mask
+      """
+      mmSystem, positions, topology = render_mm_objs_from_pdb(
+        pdb_path=pdb_filepath,
+        delete_waters=True) # remove waters for speed
+      params = mm_utils.parameters_from_openmm_system(
+        mmSystem)
+      num_particles = mmSystem.getNumParticles()
+      exceptions = params.nonbonded_exception_parameters.particles
+
+      #manual
+      max_count, dense_mask = mm.get_dense_exception_mask(
+        num_particles,
+        0,
+        exceptions)
+
+      #jax
+      symm_exceptions = mm.symmetrize_and_transpose(
+        exceptions,
+        )
+      jax_dense_mask = mm.symm_exceptions_to_dense(num_particles, exceptions)
+
+      # assert equivalent
+      assert mm.assert_equiv_masks(dense_mask, jax_dense_mask)
 
 if __name__ == '__main__':
   absltest.main()
