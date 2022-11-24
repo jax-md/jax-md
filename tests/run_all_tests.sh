0f6a662ee13da9cd20f99f85916af8612d40ec46
sschoenholz <schsam@google.com>
2022-11-23 16:44:35 -0800

    Add run_all_tests.sh.

---
+++ tests/run_all_tests.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+PYTEST_OPTS=
+RUN_DOCTEST=true
+RUN_PYTEST=true
+RUN_PYTYPE=true
+GH_VENV=false
+
+for flag in "$@"; do
+case $flag in
+  --with-cov)
+  PYTEST_OPTS+="--cov=jax-md --cov-report=xml --cov-report=term --cov-config=setup.cfg"
+  ;;
+  --help)
+  echo "Usage:"
+  echo "  --with-cov: Also generate pytest coverage."
+  exit
+  ;;
+  --no-doctest)
+  RUN_DOCTEST=false
+  ;;
+  --no-pytest)
+  RUN_PYTEST=false
+  ;;
+  --no-pytype)
+  RUN_PYTYPE=false
+  ;;
+  --use-venv)
+  GH_VENV=true
+  ;;
+  *)
+  echo "Unknown flag: $flag"
+  exit 1
+  ;;
+esac
+done
+
+# Activate cached virtual env for github CI
+if $GH_VENV; then
+  source $(dirname "$0")/../venv/bin/activate
+fi
+
+echo "====== test config ======="
+echo "PYTEST_OPTS: $PYTEST_OPTS"
+echo "RUN_DOCTEST: $RUN_DOCTEST"
+echo "RUN_PYTEST: $RUN_PYTEST"
+echo "RUN_PYTYPE: $RUN_PYTYPE"
+echo "GH_VENV: $GH_VENV"
+echo "WHICH PYTHON: $(which python)"
+echo "jax: $(python -c 'import jax; print(jax.__version__)')"
+echo "jax-md: $(python -c 'import jax_md; print(jax_md.__version__)')"
+echo "=========================="
+echo ""
+
+sh $(dirname "$0")/download_dataset_metadata.sh || exit
+
+# Instead of using set -e, we have a manual error trap that
+# exits for any error code != 5 since pytest returns error code 5
+# for no found tests. (We may force minimal test coverage in examples
+# in the future!)
+trap handle_errors ERR
+handle_errors () {
+    ret="$?"
+    if [[ "$ret" == 5 ]]; then
+      echo "error code $ret == no tests found in $egd"
+    else
+      echo "error code $ret"
+      exit 1
+    fi
+}
+
+# Run embedded tests inside docs
+if $RUN_DOCTEST; then
+  echo "=== RUNNING DOCTESTS ==="
+  # test doctest
+  sphinx-build -M doctest docs docs/_build -T
+  # test build html
+  sphinx-build -M html docs docs/_build -T
+  # test docstrings
+  pytest -n auto jax-md --doctest-modules --suppress-no-test-exit-code
+fi
+
+# # env vars must be set after doctest
+# export JAX_NUMPY_RANK_PROMOTION=raise
+# export FLAX_PROFILE=1
+# export FLAX_LAZY_RNG=1
+
+if $RUN_PYTEST; then
+  echo "=== RUNNING PYTESTS ==="
+  pytest -n auto tests $PYTEST_OPTS $PYTEST_IGNORE
+fi
+
+if $RUN_PYTYPE; then
+  echo "=== RUNNING PYTYPE ==="
+  # Validate types in library code.
+  pytype --jobs auto --config pytype.cfg jax-md/
+
+  # Validate types in examples.
+  for egd in $(find examples -maxdepth 1 -mindepth 1 -type d); do
+      # use cd to make sure pytpe cache lives in example dir and doesn't name clash
+      # use *.py to avoid importing configs as a top-level import which leads to import errors
+      # because config files use relative imports (e.g. from config import ...).
+      (cd $egd ; pytype --jobs auto --config ../../pytype.cfg "*.py")
+  done
+fi
+
+# Return error code 0 if no real failures happened.
+echo "finished all tests."
