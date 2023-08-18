46c06fbfc1d3d73400a7cc476937e6467e04c0bd
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2023-08-17 22:14:51 -0700

    Add test for gaussian thermostat

--- tests/simulate_test.py
+++ tests/simulate_test.py
@@ -654,5 +654,52 @@ class SimulateTest(test_util.JAXMDTestCase):
     self.assertAllClose(np.mean(Ts[10:]), kT, rtol=tol, atol=tol)
     self.assertTrue(not np.all(state.sigma == sigma))
 
+  @parameterized.named_parameters(test_util.cases_from_list(
+      {
+          'testcase_name': f'_dim={dim}_dtype={dtype.__name__}',
+          'spatial_dimension': dim,
+          'dtype': dtype,
+      } for dim in SPATIAL_DIMENSION
+        for dtype in DTYPE))
+  def test_nvk_ensemble(self, spatial_dimension, dtype):
+    key = random.PRNGKey(0)
+
+    box_size = quantity.box_size_at_number_density(PARTICLE_COUNT,
+                                                   f32(1.2),
+                                                   spatial_dimension)
+    displacement_fn, shift_fn = space.periodic(box_size)
+
+    bonds_i = np.arange(PARTICLE_COUNT)
+    bonds_j = np.roll(bonds_i, 1)
+    bonds = np.stack([bonds_i, bonds_j])
+
+    E = energy.simple_spring_bond(displacement_fn, bonds)
+
+    invariant = ke_fn
+
+    for _ in range(STOCHASTIC_SAMPLES):
+      key, pos_key, vel_key, T_key, masses_key = random.split(key, 5)
+
+      R = box_size * random.uniform(
+        pos_key, (PARTICLE_COUNT, spatial_dimension), dtype=dtype)
+      T = random.uniform(T_key, (), minval=0.3, maxval=1.4, dtype=dtype)
+      mass = 1 + random.uniform(masses_key, (PARTICLE_COUNT,), dtype=dtype)
+      init_fn, apply_fn = simulate.nvk(E,
+                                       shift_fn,
+                                       1e-3,
+                                       T)
+      apply_fn = jit(apply_fn)
+
+      state = init_fn(vel_key, R, mass=mass)
+
+      initial = invariant(state.momentum, state.mass)
+
+      for _ in range(DYNAMICS_STEPS):
+        state = apply_fn(state)
+
+      tol = 5e-4 if dtype is f32 else 1e-6
+      self.assertAllClose(invariant(state.momentum, state.mass), initial, rtol=tol)
+      self.assertEqual(state.position.dtype, dtype)
+      
 if __name__ == '__main__':
   absltest.main()
