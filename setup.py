c3cac0e2538908c424619d5a9dd39860eaee993e
Abhijeet Gangan <abhijeetgangan@g.ucla.edu>
2025-10-29 23:09:17 -0700

    New PyPI and github release (#359)
    * Bump minimum python version
    
    * Use uv for pypi publishing and bump python version
    
    * Bump version in setup.py
    
    * Bump requirements for docs
    
    * specify language in docs conf.py
    
    * pin minimum version of jax and jaxlib
    
    * bump jax version again ref #335

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 INSTALL_REQUIRES = [
     'absl-py',
     'numpy',
-    'jax',
-    'jaxlib',
+    'jax>=0.5.0',
+    'jaxlib>=0.5.0',
     'flax',
     'jraph',
     'einops',
@@ -39,7 +39,7 @@ INSTALL_REQUIRES = [
 
 setuptools.setup(
     name='jax-md',
-    version='0.2.8',
+    version='0.2.26',
     license='Apache 2.0',
     author='Google',
     author_email='jax-md-dev@google.com',
@@ -55,7 +55,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='Differentiable, Hardware Accelerated, Molecular Dynamics',
-    python_requires='>=3.10',
+    python_requires='>=3.11',
     classifiers=[
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
