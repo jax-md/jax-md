60b58c231f9f28737acd5ac0b24f19ab0298039a
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-11-02 15:20:28 -0800

    switch to uv

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'examples/*.py']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
