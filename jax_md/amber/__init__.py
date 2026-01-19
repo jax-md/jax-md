b742d4aea0667c169837790dde1c52e3381d4837
alecbetancourt <alecbetancourt@gmail.com>
2026-01-19 12:42:42 -0500

    Remove old AMBER files

--- jax_md/amber/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import amber_energy_v2
-from . import amber_fe
-from . import amber_forcefield
-from . import amber_helper
-from . import amber_interactions
-from . import amber_restraints
-from . import amber
