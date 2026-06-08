b7e0b3f560ada68579895b85359dd5449388fc2e
Kyle Daniel Miller <kmiller@citrine.io>
2026-06-08 16:00:09 -0500

    Accelerate a2c subcell generation function (#398)
    * added accelerated version of get_subcells_to_crystallize called get_subcells_to_crystallize_parallel which is backward compatible and contains two speedups: (1) reducing the number of invalid potential subcells considered, and (2) paralellizing the search for subcells. Enables at least 2x speedup for reasonably intensive problems and larger speedups as the problem size increases. Also added an a2c test which compares the new method to the original method to confirm they generate the same number of subcells
    
    * fixed lint error that slipped through
    
    * more formatting fixes
    
    * made parallel version of subcell crystallizer the default in a2c workflow
    
    * removed timing from a2c test
    
    * reduced subcell sampling density in a2c test to make it faster

---
+++ tests/a2c_test.py
@@ -0,0 +1,76 @@
+import os
+
+import jax
+import pymatgen as mg
+from absl.testing import absltest, parameterized
+
+from jax_md.a2c.crystallizer_utils import (
+  get_subcells_to_crystallize,
+  get_subcells_to_crystallize_parallel,
+)
+
+jax.config.update('jax_enable_x64', True)
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+AMORPHOUS_SI64_PATH = os.path.join(DATA_DIR, 'amorphous_si64.cif')
+
+
+class A2CTest(parameterized.TestCase):
+  """Test the A2C workflow."""
+
+  def test_subcells_to_crystallize_parallel(self):
+    """Test the parallel version of the subcells to crystallize function."""
+    amorphous_structure = mg.core.Structure.from_file(AMORPHOUS_SI64_PATH)
+
+    # Run both versions of the crystallize function, timing each, and compare the results
+    d_frac = 0.2
+    nmin = 1
+    nmax = 12
+    n_workers = os.cpu_count() - 1
+
+    orig = get_subcells_to_crystallize(
+      amorphous_structure, d_frac=d_frac, nmin=nmin, nmax=nmax
+    )
+    orig = sorted(orig, key=lambda x: (tuple(x[0]), tuple(x[1]), tuple(x[2])))
+
+    improved_subcell_generation_results = get_subcells_to_crystallize_parallel(
+      amorphous_structure, d_frac=d_frac, nmin=nmin, nmax=nmax, n_workers=1
+    )
+    improved_subcell_generation_results = sorted(
+      improved_subcell_generation_results,
+      key=lambda x: (tuple(x[0]), tuple(x[1]), tuple(x[2])),
+    )
+
+    if n_workers > 1:
+      parallel_and_improved_crystallize_results = (
+        get_subcells_to_crystallize_parallel(
+          amorphous_structure,
+          d_frac=d_frac,
+          nmin=nmin,
+          nmax=nmax,
+          n_workers=n_workers,
+        )
+      )
+      parallel_and_improved_crystallize_results = sorted(
+        parallel_and_improved_crystallize_results,
+        key=lambda x: (tuple(x[0]), tuple(x[1]), tuple(x[2])),
+      )
+
+    # Compare results to confirm consistency between original and improved subcell generation functions
+    self.assertEqual(
+      len(orig),
+      len(improved_subcell_generation_results),
+      f'Original function and improved subcell generation function returned different number of subcells: {len(orig)} != {len(improved_subcell_generation_results)}',
+    )
+    if n_workers > 1:
+      self.assertEqual(
+        len(orig),
+        len(parallel_and_improved_crystallize_results),
+        f'Original function and parallel & improved crystallize function returned different number of subcells: {len(orig)} != {len(parallel_and_improved_crystallize_results)}',
+      )
+    else:
+      print('Parallel function not tested because n_workers <= 1')
+
+
+if __name__ == '__main__':
+  absltest.main()
